aea241f4933e9da799e2709ed2a4fb7e51e9b154
hyperpro <zhangbjb@126.com>
2020-03-03 11:42:39 -0600

    XZ macOS update

--- server.py
+++ server.py
@@ -78,6 +78,6 @@ def save_data():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='linux.cs.uchicago.edu', port=5000)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='linux.cs.uchicago.edu', port=5000)
+    # app.run(debug=True, host='127.0.0.1', port=5000)
 
