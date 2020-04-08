504c859b6c645dc7f0021aed40779451c376ca30
ZhongnianTao <31367011+ZhongnianTao@users.noreply.github.com>
2020-04-08 21:33:36 +0800

    add login function

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for, session
 from youtube_dl import YoutubeDL
 import time
+from flask_pymongo import PyMongo
+import bcrypt
 
 app = Flask(__name__, template_folder='templates')
 
+app.config['MONGO_DBNAME'] = 'admin'
+app.config['MONGO_URI'] = 'mongodb://uchivideo1:63czhj6c8pe@18.179.39.139:27017/admin'
+
+mongo = PyMongo(app)
 
 def get_time():
     return str(hash(str(time.time())))
-
 
 def extract_url(youtube_id):
     ydl = YoutubeDL()
@@ -80,13 +85,13 @@ def extract_url(youtube_id):
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
-
+    if 'username' in session:
+        return render_template('home.html', userinfo='You are logged in as ' + session['username'] + '.')
+    return render_template('home-not-logged-in.html')
 
 @app.route('/home')
 def homepage():
     return render_template('video_player.html')
-
 
 @app.route('/play/<string:page_name>/')
 def render_video(page_name):
@@ -97,10 +102,63 @@ def render_video(page_name):
 def post_survey():
     return render_template('post_survey.html')
 
-
 @app.route('/signup/')
 def sign_up():
-    return render_template('sign-up.html')
+    return render_template('sign-up-ver2.html')
+
+@app.route('/signuprequest', methods=['POST'])
+def signuprequest():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert(
+                {
+                    'name':request.form['username'], 
+                    'password': hashpass, 
+                    'firstname': request.form['firstname'],
+                    'lastname': request.form['lastname'],
+                    'email': request.form['email'],
+                    'address': request.form['address'],
+                    'address2': request.form['address2'],
+                    'age': request.form['age'],
+                    'device': request.form['device'],
+                    'isp': request.form['isp'],
+                    'type': request.form['type'],
+                    'plan': request.form['plan'],
+                    'inlineRadioOptions': request.form['inlineRadioOptions'],
+                    'timespent': request.form['timespent'],
+                    'resolution': request.form['resolution'],
+                    'msize': request.form['msize']
+               }
+            )
+            session['username'] =  request.form['username']
+            return redirect(url_for('hello_world'))
+
+        return 'That username already exists!'
+
+    return render_template('sign-up-ver2.html')
+
+
+@app.route('/loginpage')
+def loginpage():
+    if 'username' in session:
+        return redirect(url_for(hello_world))
+    return render_template('login-page.html')
+    
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name': request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('hello_world'))
+
+    return 'Invalid username or password'
 
 @app.route('/post/', methods=['POST'])
 def save_data():
@@ -109,4 +167,5 @@ def save_data():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(debug=True, host='127.0.0.1', port=5000)
